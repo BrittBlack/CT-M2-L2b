2bcce5b32d693ed88ee94fd54cabdce33505e580
BrittBlack <brittneysblackmon@gmail.com>
2024-05-01 21:53:55 -0500

    first commit

---
+++ Exercise1.py
@@ -0,0 +1,22 @@
+#1. Nested Decisions:The Adventure Game
+#
+#
+#
+#Task 1: Code Correction
+
+place = input("Choose a place: forest or cave? ")
+action = input("climb a tree or cross a river? ")
+action2 = input("light a torch or proceed in the dark? ")
+
+if place == "forest":
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    else:
+        print("You found a boat!")
+elif place == "cave":
+     print("You find a hidden treasure!")
+
+if action2 == "light a torch": #Task 2: Setting the Scene
+            print("You found a bug!")
+else:
+    print("You can't see a thing in the dark.")
