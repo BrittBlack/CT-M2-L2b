b8daf02b88c5f83158ce2e29e4c4f3fe74960ada
BritBritB <brittneysblackmon@gmail.com>
2024-05-02 13:09:59 -0500

    updated exercise

--- Exercise2.py
+++ Exercise2.py
@@ -3,13 +3,12 @@
 #
 #
 #
-# attendees = int(input("Enter number of attendees:  "))
-
-# if attendees > 100:
-#     print("large hall")
-#     if attendees < 100:
-#         print("conference room")
-    
-
 attendees = int(input("Enter number of attendees: "))
+food_preference = input("Type of food for event: (vegetarian/other) ")
 print("large hall") if attendees > 100 else print("conference room")
+print("projector") if attendees > 100 else print("audio system")
+if food_preference == "vegetarian":
+    print("Try Veggie Delight Caterers.")
+else:
+    print("Try Gourmet Meals Caterers.")
+
