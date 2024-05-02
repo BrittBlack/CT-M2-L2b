316b7e7c9d4eb607908b017e98f93b54e8b636bf
BritBritB <brittneysblackmon@gmail.com>
2024-05-02 12:58:23 -0500

    updated exercise

--- Exercise1.py
+++ Exercise1.py
@@ -3,31 +3,18 @@
 #
 #
 #Task 1: Code Correction
-#1st Attempt
-# place = input("Choose a place: forest or cave? ")
-# action = input("climb a tree or cross a river? ")
-# action2 = input("light a torch or proceed in the dark? ")
 
-# if place == "forest":
-#     if action == "climb a tree":
-#         if action2 == "light a torch":
-#     print("You found a bird's nest!")
-#           print("You found a bug!")
-    
-#     else:
+# place = input("Choose a place: forest or cave? ")
+
+# if place = "forest":
+#     action = input("climb a tree or cross a river?")
+#     if action = "climb a tree":
+#         print("You found a bird's nest!")
+#     else action = "cross a river":
 #         print("You found a boat!")
-    
-#     if action2 == "light a torch": #Task 2: Setting the Scene
-          
-#     else:
-#         print("You can't see a thing in the dark.")
- 
-# elif place == "cave":
-#      print("You find a hidden treasure!")
-#
-#
-#
-#2nd attempt
+# elif place = "cave":
+#     print("You find a hidden treasure!")
+
 
 
 place = input("Choose a place: forest or cave? ")
@@ -36,10 +23,17 @@ if place == "forest":
     action = input("climb a tree or cross a river?")
     if action == "climb a tree":
         print("You found a bird's nest!")
-    if action == "cross a river":
-        print("You found a boat!")
+        if action == "cross a river":
+            print("You found a boat!")
 elif place == "cave":
-    print("You find a hidden treasure!")
+    action2 = input("light a torch or proceed in the dark?")
+    pass    
+    if action2 == "light a torch":
+        print("You can see.")
+        pass
+        if action2 == "proceed in the dark":
+            print("You cannot see.")
+else:
+     print("You find a hidden treasure!")      
 
 
- 
